a3bb05de12bbebdd52e46b1b806bf548c38541e6
baolibin <2578858653@qq.com>
2020-12-01 22:46:31 +0800

    clone 深拷贝

--- src/main/code/lang/Object.java
+++ src/main/code/lang/Object.java
@@ -6,8 +6,8 @@ package lang;
  * <p>
  * Purpose :
  */
-public class Object {
-    public static void main(String[] args) {
+public class Object implements Cloneable {
+    public static void main(String[] args) throws CloneNotSupportedException {
         java.lang.Object obj1 = new java.lang.Object();
         java.lang.Object obj2 = new java.lang.Object();
         System.out.println(obj1.hashCode());
@@ -15,5 +15,31 @@ public class Object {
         System.out.println(obj1 == obj2);
         System.out.println(obj1.equals(obj2));
 
+        ObjDemo ObjDemo1 = new ObjDemo();
+        ObjDemo1.setName("fly");
+        ObjDemo ObjDemo2 = ObjDemo1.clone();
+        System.out.println("ObjDemo1 == ObjDemo2 " + (ObjDemo1 == ObjDemo2));
+        System.out.println("ObjDemo1.equals(ObjDemo2 " + (ObjDemo1.equals(ObjDemo2)));
+        System.out.println(ObjDemo1.getName());
+        System.out.println(ObjDemo2.getName());
+    }
+}
+
+class ObjDemo implements Cloneable {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    protected ObjDemo clone() throws CloneNotSupportedException {
+        ObjDemo objDemo = new ObjDemo();
+        return (ObjDemo) super.clone();
     }
 }
